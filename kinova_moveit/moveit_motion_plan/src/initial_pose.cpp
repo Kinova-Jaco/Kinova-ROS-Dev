fc3065e723f7d1e77719723edf1e0af52afa266b
Longfei Zhao <lzhao@kinova.ca>
2016-07-14 17:00:33 -0400

    setFromIK working: Cartesian-->Joint

--- kinova_moveit/moveit_motion_plan/src/initial_pose.cpp
+++ kinova_moveit/moveit_motion_plan/src/initial_pose.cpp
@@ -38,6 +38,7 @@ int main(int argc, char** argv)
     tf_retract.setOrigin(tf::Vector3(0.028, -0.1714, 0.2995));
     tf_retract.setRotation(tf::Quaternion(0.68801, -0.5464, 0.32995, 0.34528));
     geometry_msgs::Pose Retract = to_geometry_pose(tf_retract);
+    double retract_joint_value[6] = {-1.57079622, 2.61799388,  0.4712389 , -1.6057028 ,  0.08726646, 1.74532925};
 
     // Home (ready pose)
     tf::Pose tf_home;
@@ -53,8 +54,16 @@ int main(int argc, char** argv)
     geometry_msgs::Pose Random = to_geometry_pose(tf_random_pose);
 
 
+
+
+    ROS_WARN("---------------- Initial state -------------");
+
     moveit::planning_interface::MoveGroup group("arm");
-    moveit::planning_interface::PlanningSceneInterface scene;
+    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+    robot_model::RobotModelPtr robot_model_ptr=robot_model_loader.getModel();
+    robot_state::RobotStatePtr robot_state_ptr(group.getCurrentState());
+    robot_state_ptr->setToDefaultValues(robot_state_ptr->getJointModelGroup(group.getName()), "home");
+    robot_state_ptr->update();
 
     group.setGoalJointTolerance(0.1*M_PI/180);
     group.setGoalPositionTolerance(0.001);
@@ -63,84 +72,55 @@ int main(int argc, char** argv)
     // getPlanningFrame() is: /root
     ROS_INFO_STREAM("getPlanningFrame() is: " << group.getPlanningFrame());
 
-    std::vector<std::string> names = group.getActiveJoints();
-    for (int i=0; i<names.size(); i++)
-    {
-        ROS_INFO_STREAM("Active joints: " << names[i]);
-    }
-
-    ROS_WARN("---------------- Initial state -------------");
-
-    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
-    robot_model::RobotModelPtr robot_model_ptr=robot_model_loader.getModel();
-    robot_state::RobotStatePtr robot_state_ptr(group.getCurrentState());
-    robot_state_ptr->setToDefaultValues(robot_state_ptr->getJointModelGroup(group.getName()), "home");
-
-    ROS_WARN("DefaultPlannedID is: %s.", group.getDefaultPlannerId().c_str());
-
-
     ROS_INFO_STREAM("getCurrentPose() is: " << group.getCurrentPose("j2n6s300_end_effector").pose );
     ROS_INFO_STREAM("printStatePositions() is: ");
     group.getCurrentState()->printStatePositions();
 
 
-    ROS_WARN("---------------- Set to Home state -------------");
+
+    ROS_WARN("---------------- Setting to Home state -------------");
 
     robot_state::RobotState home_state = *group.getCurrentState();
-    // joint value of Home pose (ready pose)
-//    double home_joint_value[6] = {-1.47550822, 2.92151711,  1.00484714, -2.08487422,  1.44703677, 1.31851771};
-//    home_state.setVariablePositions(home_joint_value);
-    ROS_WARN_STREAM("Home is: " << Home);
-
-    home_state.setFromIK(home_state.getJointModelGroup(group.getName()), Home);
-    ROS_WARN("Home state is: ");
+    home_state.setVariablePositions(home_joint_value);
+    ROS_INFO("Home state is defined: ");
     home_state.printStatePositions();
 
     group.setJointValueTarget(home_state);
     group.move();
     sleep(5);
-    std::vector<double> current_joint = group.getCurrentJointValues();
-    ROS_INFO("home joint is: %f, %f, %f, %f, %f, %f.", current_joint[0],current_joint[1], current_joint[2], current_joint[3], current_joint[4],current_joint[5]);
-    ROS_INFO_STREAM("home pose is: " << group.getCurrentPose().pose);
+
+    ROS_INFO("home joint reached:");
+    group.getCurrentState()->printStatePositions();
+    ROS_INFO_STREAM("home pose reached: " << group.getCurrentPose().pose);
 
 
-    ROS_WARN("---------------- After planing goal -------------");
+
+    ROS_WARN("---------------- Moving to goal -------------");
 
     robot_state::RobotState goal_state = *group.getCurrentState();
 
-    // setFromIK() does not work here?
-    /*
-    geometry_msgs::Pose goal;
-    tf::poseTFToMsg(tf_retract, goal);
-    group.setPoseTarget(goal);
-    goal_state.setFromIK(goal_state.getJointModelGroup(group.getName()), goal);
-    */
-
-    // joint value of Retract pose (rest pose)
-    double goal_joint_value[6] = {-1.57079622, 2.61799388,  0.4712389 , -1.6057028 ,  0.08726646, 1.74532925};
-    goal_state.setVariablePositions(goal_joint_value);
+    goal_state.setFromIK(goal_state.getJointModelGroup(group.getName()), Retract,10,1);
+    goal_state.update();
 
     group.setJointValueTarget(goal_state);
 
-    ROS_INFO("goal in Joint space: ");
-    goal_state.printStatePositions();
+    ROS_INFO("Goal reached: ");
+    group.getCurrentState()->printStatePositions();
+
 
 
     ROS_WARN("----------- Moving to goal (group.move) ----------");
     moveit::planning_interface::MoveGroup::Plan my_plan;
     if(group.plan(my_plan))
     {
-        ROS_WARN("Success in group.plan(my_plan)");
+        ROS_INFO("Success in group.plan(my_plan)");
         group.move();
         sleep(5);
     }
 
-    ROS_WARN("----------- After execution ----------");
-
-    ROS_INFO_STREAM("GoalJointTolerance is: " << group.getGoalJointTolerance() );
-    ROS_INFO_STREAM("printStatePositions() is: ");
+    ROS_INFO_STREAM("Goal joint reached: ");
     group.getCurrentState()->printStatePositions();
-//    ROS_INFO_STREAM("After execution pose is: " << group.getCurrentPose("j2n6s300_end_effector").pose );
+    ROS_INFO_STREAM("Goal pose reached: " << group.getCurrentPose("j2n6s300_end_effector").pose );
 
     return 0;
 }
