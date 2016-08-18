c0087678071f21bc887a8b53dca3978e31abb0fc
Longfei Zhao <lzhao@kinova.ca>
2016-08-18 09:19:38 -0400

    change duration to wallduration

--- kinova_moveit/moveit_motion_plan/src/gripper_command_action_server.cpp
+++ kinova_moveit/moveit_motion_plan/src/gripper_command_action_server.cpp
@@ -39,7 +39,7 @@ GripperCommandActionController::GripperCommandActionController(ros::NodeHandle &
             ROS_WARN("Waited for the kinova driver for 30 seconds, but it never showed up. Continue waiting the finger state on topic /j2n6s300_driver/out/finger_position ...");
             started_waiting_for_controller = ros::Time(0);
         }
-        ros::Duration(0.1).sleep();
+        ros::WallDuration(0.1).sleep();
     }
 
 
